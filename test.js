7d22f7fae7d7cc9b3363f02d389854702274e4b5
Miracle Okafor <30794063+devMiracle@users.noreply.github.com>
2018-05-24 11:09:38 +0100

    update test.js

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 const timeToWord = require('./index');
 
 // console.log(timeToWord);
-let timeInWord = timeToWord.getTimeInWord(2,"");
+let timeInWord = timeToWord.getTimeInWord(2,24);
 if(timeInWord.status){
+    //if timeInWord.status === true
     console.log(`Hour : ${timeInWord.hour}  Minute : ${timeInWord.minute}  ===>  ${timeInWord.time}`);    
 }else{
+    //if timeInWord.status === false
     console.log(timeInWord.error);
 }
